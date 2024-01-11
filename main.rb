e0a8ba327799d4c795e4f02359749dae07e2fed2
ryo <ta090972@icloud.com>
2024-01-11 15:40:11 +0900

    <リファクタリング>  1.メソッド抽出（read_memos,write_memos）  2.URIの修正  3.メソッド変更（追加はPOST、編集はPATCH、削除はDELETE）
    <機能追加>
     1.題名の空欄は受け付けない（input(text)内にrequiredを追加）

--- main.rb
+++ main.rb
@@ -3,11 +3,8 @@ require 'sinatra/reloader'
 require 'net/http'
 require 'json'
 
-enable :method_override
-
 get '/memos' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
+  memos = read_memos
 
   @memos = ''
   memos.each do |title, _|
@@ -15,52 +12,40 @@ get '/memos' do
     @memos = @memos + "<li><a href=\"/memos/#{title}\">#{title}</a></li>"
     @memos = @memos + "</ul>"
   end
-
   erb :memos
 end
 
 post '/memos' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
+  memos = read_memos
+  memos[params[:title]] = params[:content]
+  write_memos(memos)
+  redirect '/memos'
+end
+
+patch '/memos' do
+  memos = read_memos
   old_title = params[:old_title]
   new_title = params[:new_title]
   new_content = params[:new_content]
-
   memos[new_title] = memos.delete(old_title)
   memos[new_title] = new_content
-  memos_json = JSON.generate(memos)
-
-  File.open(path, 'w') do |file|
-    file.write(memos_json)
-  end
-
+  write_memos(memos)
   redirect '/memos'
 end
 
 delete '/memos' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
+  memos = read_memos
   targeted_title = params[:title]
   deleted_memos = memos.delete_if { |title, _| title == targeted_title }
-  memos_json = JSON.generate(deleted_memos)
-  
-  File.open(path, 'w') do |file|
-    file.write(memos_json)
-  end
-
+  write_memos(deleted_memos)
   redirect '/memos'
 end
 
 get '/memos/:title' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
+  memos = read_memos
   title = params[:title]
-
-  if memos.key?(title)
-    @title = title
-    @content = memos[title]
-  end
-
+  @title = title
+  @content = memos[title]
   erb :show_memos
 end
 
@@ -68,28 +53,28 @@ get '/new' do
   erb :new
 end
 
-post '/save_memos' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
-  memos[params[:title]] = params[:content]
+get '/edit' do
+  memos = read_memos
+  title = params[:title]
+  @title = title
+  @content = memos[title]
+  erb :edit_memos
+end
+
+def read_memos
+  path = read_memos_file_path
+  File.exist?(path) ? JSON.parse(File.read(path)) : {}
+end
+
+def write_memos(memos)
+  path = read_memos_file_path
   memos_json = JSON.generate(memos)
 
   File.open(path, 'w') do |file|
     file.write(memos_json)
   end
-
-  redirect '/memos'
 end
 
-get '/edit' do
-  path = File.join(Dir.pwd, 'save_memos.json')
-  memos = File.exist?(path) ? JSON.parse(File.read(path)) : {}
-  title = params[:title]
-
-  if memos.key?(title)
-    @title = title
-    @content = memos[title]
-  end
-
-  erb :edit_memos
+def read_memos_file_path
+  File.join(Dir.pwd, 'save_memos.json')
 end
